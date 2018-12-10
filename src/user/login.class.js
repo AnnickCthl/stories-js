9988acbe7855e2e715ce720b17606ee66c23b587
Annick <annick.cathala@pm.me>
2018-12-10 14:39:49 +0100

    Formulaire + Bouton

---
+++ src/user/login.class.js
@@ -0,0 +1,39 @@
+class Login {
+    constructor() {
+        //Modifier le document HTML
+        $(document).attr('title', 'Identification') // Dans le document .html, trouve moi l'attribut Title et remplace-le part "Identification"
+        // Modifier le titre de la page (le h1 qui porte l'Id main-title)
+        $('#main-title').html('Identifiez-vous')
+        //Définir du listener sur le formulaire
+        this.formListener();
+
+    }
+
+    /**
+     *  un gestionnaire d'événement sur le formulaire de login
+     * @param void
+     * @return void
+     */
+    formListener() {
+        $('#loginForm').on(  //Place moi un gestionnaire d'événement sur le formulaire "loginForm"
+            'keyup', //Nom de l'événement
+            //callback fonction appellée si l'événement survient.
+            function (event) {
+                //Il faut vérifier que les 2 champs ne sont pas vides pour activer le bouton.
+                const login = $('[name="loginField"]');
+                const password = $('[name="passwordField"]');
+                // console.log('Login : ' + login.val());
+                // Est-ce que les deux champs sont remplis ?
+
+                if (login.val() !== '' && password.val() !== ''){
+                    //On peut activer le bouton
+                    $('#btnLogin').removeAttr('disabled');
+                }
+                else{
+                    $('#btnLogin').attr('disabled', 'disabled');
+                }
+            }
+
+        )
+    }
+}
