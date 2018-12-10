01ff36e901149465ddf57a938e968309d7227aa4
Annick <annick.cathala@pm.me>
2018-12-10 10:29:42 +0100

    Externalisation des scripts

---
+++ assets/javascript/main.js
@@ -0,0 +1,6 @@
+/**
+ * @name main.js
+ * @description Point d'entrée principal dans l'application JS.
+ */
+let title = document.getElementById('main-title');
+title.innerHTML = 'Hello Javascript !';
