d735210e08e32cb0f178e5c8d670532303057271
Annick <annick.cathala@pm.me>
2018-12-12 14:21:18 +0100

    exclusion build

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -5,15 +5,19 @@
 import { Login } from './../../src/user/login.class';
 import { LoginController } from '../../src/user/login/login.Controller.class';
 import { StoriesController } from '../../src/stories/storiesController.class';
+import { UserService } from '../../src/services/user-services.class';
 let title = document.getElementById('main-title');
 title.innerHTML = 'Hello Javascript !';
 
 //@version 1.0.1 Passage par contrôleur
-const controller = new LoginController();
-controller.getView();
+const authGuard = new UserService();
+if (!authGuard.hasUser()) {
+    const controller = new LoginController();
+    controller.getView();
 
-//Créer une instance de Login
-const login = new Login();
-
-//const stories = new StoriesController();
-//stories.getView();
+    //Créer une instance de Login
+    const login = new Login();
+} else {
+    const stories = new StoriesController();
+    stories.getView();
+}
